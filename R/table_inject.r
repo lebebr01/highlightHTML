62f95901da66fd3d8bcff7dbcf650eea124a39f7
Brandon LeBeau <lebebr01@gmail.com>
2016-10-24 11:16:27 -0500

    finalize table inject and bump version

--- R/table_inject.r
+++ R/table_inject.r
@@ -7,6 +7,20 @@
 #' @param conditions A vector of conditions to include id. Must be same length as id
 #' @param variable An optional vector of column names to specify search of conditions.
 #' @export
+#' 
+#' @examples 
+#' library(dplyr)
+#' library(highlightHTML)
+#'
+#' summ_table <- mtcars %>% 
+#'   group_by(cyl) %>% 
+#'   summarise(avg_mpg = mean(mpg), sd_mpg = sd(mpg)) %>%
+#'   data.frame()
+#'   
+#'   table_id_inject(summ_table, id = c('#bgred', '#bgblue'), 
+#'   conditions = c('< 2', '> 16'))
+#'   
+
 table_id_inject <- function(x, id, conditions, variable = NULL) {
   
   if(is.null(variable)) {
@@ -21,9 +35,12 @@ table_id_inject <- function(x, id, conditions, variable = NULL) {
     ifelse(eval(parse(text = paste_cond[[xx]])), TRUE, FALSE))
   
   x2 <- x
-  lapply(seq_along(locations), function(xx) 
-      x2[locations[[xx]] == TRUE] <- paste(x2[locations[[xx]] == TRUE], id[xx])
-    )
+  for(xx in seq_along(locations)) {
+    x2[locations[[xx]] == TRUE] <- paste(x2[locations[[xx]] == TRUE], id[xx])
+  }
+  # lapply(seq_along(locations), function(xx) 
+  #     x2[locations[[xx]] == TRUE] <- paste(x2[locations[[xx]] == TRUE], id[xx])
+  #   )
   
-  list(x, x2)
+  return(x2)
 }
